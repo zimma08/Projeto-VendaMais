a548f26d0b533f810daeea9a3a152c626b002ee4
Richlin15 <caua.richlin@univille.br>
2026-05-07 21:49:53 -0300

    Adicionando Functions

--- src/function_app.py
+++ src/function_app.py
@@ -10,3 +10,21 @@ def timer_trigger_aula02(myTimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed.')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger_aula03(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Teste function 1.')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger_aula04(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('teste function 2.')
