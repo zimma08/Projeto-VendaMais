10839b1cc0406fe937dd406c4b24c0a720f4100b
Richlin15 <caua.richlin@univille.br>
2026-06-02 20:11:54 -0300

    ajuste 2 banco de dados

--- src/triggers/extract_entrega.py
+++ src/triggers/extract_entrega.py
@@ -1,9 +1,50 @@
-import logging
 import azure.functions as func
+import logging
+import os
+import pyodbc
 
 bp = func.Blueprint()
 
-@bp.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=False) 
-def extract_entrega(myTimer: func.TimerRequest) -> None:
-    logging.info('tabela entrega.')
+@bp.timer_trigger(schedule="0 0 6 * * *", arg_name="timer", run_on_startup=False)
+def extract_entrega(timer: func.TimerRequest) -> None:
+    
+    sql_server = os.getenv("SQL_SERVER_SOURCE")
+    sql_database = os.getenv("SQL_DATABASE_SOURCE")
+    sql_user = os.getenv("SQL_USER_SOURCE")
+    sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
+
+    logging.info(f"servidor: {sql_server},  banco: {sql_database}, usuario:{sql_user}, senha: {sql_pass} ...")
+
+
+    # Configura a string de conexão para o banco de dados SQL Server
+    conn_str = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={sql_server};"
+        f"DATABASE={sql_database};"
+        f"UID={sql_user};"
+        f"PWD={sql_pass};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+
+   
+    try:
+        # Estabelece a conexão com o banco de dados usando pyodbc
+        with pyodbc.connect(conn_str) as conn:
+            # Cria um cursor para executar a consulta   
+            cursor = conn.cursor()
+            
+            query = "select top 5 * from erp.entrega"
+
+            # Executa a consulta SQL
+            cursor.execute(query)
+
+            # Busca todos os resultados da consulta
+            rows = cursor.fetchall()
+
+            logging.info(rows)           
+
+    except Exception as e:
+        logging.error(f"Erro ao ler erp.entrega: {str(e)}")
+        raise
