4bc2d448c990471f4eee5d58ce15b6ec4d50871e
Richlin15 <caua.richlin@univille.br>
2026-06-17 18:36:41 -0300

    Ajuste Insert tabela Regiao

--- src/triggers/extract_regiao.py
+++ src/triggers/extract_regiao.py
@@ -13,6 +13,11 @@ def extract_regiao(timer: func.TimerRequest) -> None:
     sql_user = os.getenv("SQL_USER_SOURCE")
     sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
 
+    sql_server_dest = os.getenv("SQL_SERVER_DEST")
+    sql_database_dest = os.getenv("SQL_DATABASE_DEST")
+    sql_user_dest = os.getenv("SQL_USER_DEST")
+    sql_pass_dest = os.getenv("SQL_PASSWORD_DEST")
+
     logging.info(f"servidor: {sql_server},  banco: {sql_database}, usuario:{sql_user}, senha: {sql_pass} ...")
 
 
@@ -28,14 +33,24 @@ def extract_regiao(timer: func.TimerRequest) -> None:
         "Connection Timeout=30;"
     )
 
-   
+    conn_str_dest = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={sql_server_dest};"
+        f"DATABASE={sql_database_dest};"
+        f"UID={sql_user_dest};"
+        f"PWD={sql_pass_dest};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+
     try:
         # Estabelece a conexão com o banco de dados usando pyodbc
         with pyodbc.connect(conn_str) as conn:
             # Cria um cursor para executar a consulta   
             cursor = conn.cursor()
             
-            query = "select top 5 * from erp.regiao"
+            query = "select * from erp.regiao"
 
             # Executa a consulta SQL
             cursor.execute(query)
@@ -43,7 +58,43 @@ def extract_regiao(timer: func.TimerRequest) -> None:
             # Busca todos os resultados da consulta
             rows = cursor.fetchall()
 
-            logging.info(rows)           
+            with pyodbc.connect(conn_str_dest) as conn_dest:
+                cursor_dest = conn_dest.cursor()
+
+                for row in rows:
+
+                    cursor_dest.execute(
+                        """
+                        INSERT INTO erp.regiao
+                        (
+                            cd_regiao,
+                            nm_regiao,
+                            sg_uf,
+                            nm_cidade,
+                            fl_ativo,
+                            dt_inclusao,
+                            dt_atualizacao,
+                            nm_sistema_origem,
+                            cd_registro_origem
+                        )
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                        """,
+                        row.cd_regiao,
+                        row.nm_regiao,
+                        row.sg_uf,
+                        row.nm_cidade,
+                        row.fl_ativo,
+                        row.dt_inclusao,
+                        row.dt_atualizacao,
+                        row.nm_sistema_origem,
+                        row.cd_registro_origem
+                    )
+
+                conn_dest.commit()
+
+            logging.info(
+                f"{len(rows)} registros inseridos em regiao"
+            )          
 
     except Exception as e:
         logging.error(f"Erro ao ler erp.regiao: {str(e)}")
